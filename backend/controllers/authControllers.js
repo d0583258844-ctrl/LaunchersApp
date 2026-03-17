88a53b4fa04a403ffee4f5d2db5c9779779fd155
Daniel Levy <d0583258844@gmail.com>
2026-03-17 17:08:53 +0200

    add all the functions of the users, except the get current user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -8,14 +8,12 @@ export const createUser = async (req, res) => {
         if (!username || !password || !email || !user_type) {
             return res.status(400).send("One field or more is missing.")
         }
-
         const userExsits = await User.findOne({ user_type: user_type })
         if (userExsits) {
             res.send(`Error: User with user type: ${user_type} alreday exsits`)
-
-            const newUser = await User.create({ username, password, email, user_type, last_login })
-            res.status(201).json({ message: "User created seccefully", user: newUser })
         }
+        const newUser = await User.create({ username, password, email, user_type, last_login })
+        res.status(201).json({ message: "User created seccefully", user: newUser })
     } catch (error) {
         res.status(500).json({ message: "Failed", error: error })
     }
@@ -30,6 +28,7 @@ export const login = async (req, res) => {
         }
 
         const user = await User.findOne({ username: username })
+        user.last_login = new Date()
         if (!user) {
             return res.status(401).json({ message: `Error: User with user name: ${username} Not Found` })
         }
@@ -43,20 +42,37 @@ export const login = async (req, res) => {
 }
 
 export const deleteUser = async (req, res) => {
-    const { id } = res.query
+    const { id } = req.query
+    const user = await User.findById({ _id: id })
+
     try {
+        if (!user) {
+            return res.status(402).json({ message: `User with id: ${id} Not Found` })
+        }
+        await User.deleteOne({ _id: user.id })
+        res.status(203).json({ message: "User deleted seccessfuly", user: user })
 
     } catch (error) {
-
+        res.status(500).json({ message: "server error", error: error.message })
     }
 }
 
 export const updateUser = async (req, res) => {
-    const { username, password, email, user_type, last_login } = req.body
+    const { id, username, password, email, user_type } = req.body
+    const user = await User.findById(id)
     try {
+        if (!user) {
+            res.status(400).json({ message: "User not found" })
+        }
+        const newUserData = { username: username || user.username, password: password || user.password, email: email || user.email, user_type: user_type || user.user_type, last_login: user.last_login, _id: user._id }
+        const updatedUser = await User.findByIdAndUpdate(id, newUserData)
+        console.log(updatedUser);
+        console.log(newUserData);
+
+        res.status(201).json({ message: "Updated", info: newUserData })
 
     } catch (error) {
-
+        res.status(500).send(error.message)
     }
 }
 
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -21,44 +21,44 @@ export const auth = (roles) =>  (req, res, next) => {
     }
 }
 
-export const checkAuthIntelligence = (req, res, next) => {
-    const token = req.cookies.token
-    try {
-        if (!token) {
-            res.status(401).send("No token given. unauthraized")
-        }
-        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-            if (err) {
-                res.status(402).json({ message: "error", info: err.message })
-            }
-            if (decoded?.user_type !==  "intelligence force user") {
-                res.status(401).send("Only administrator can create a new user")
-            }
-            req.user = decoded.id
-        })
-        next()
-    } catch (error) {
-        res.status(500).json({ message: "error", info: error.message })
-    }
-}
+// export const checkAuthIntelligence = (req, res, next) => {
+//     const token = req.cookies.token
+//     try {
+//         if (!token) {
+//             res.status(401).send("No token given. unauthraized")
+//         }
+//         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+//             if (err) {
+//                 res.status(402).json({ message: "error", info: err.message })
+//             }
+//             if (decoded?.user_type !==  "intelligence force user") {
+//                 res.status(401).send("Only administrator can create a new user")
+//             }
+//             req.user = decoded.id
+//         })
+//         next()
+//     } catch (error) {
+//         res.status(500).json({ message: "error", info: error.message })
+//     }
+// }
 
-export const checkAuthAirForce = (req, res, next) => {
-    const token = req.cookies.token
-    try {
-        if (!token) {
-            res.status(401).send("No token given. unauthraized")
-        }
-        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-            if (err) {
-                res.status(402).json({ message: "error", info: err.message })
-            }
-            if (decoded?.user_type !==  "air force user") {
-                res.status(401).send("Only  can create a new user")
-            }
-            req.user = decoded.id
-        })
-        next()
-    } catch (error) {
-        res.status(500).json({ message: "error", info: error.message })
-    }
-}
+// export const checkAuthAirForce = (req, res, next) => {
+//     const token = req.cookies.token
+//     try {
+//         if (!token) {
+//             res.status(401).send("No token given. unauthraized")
+//         }
+//         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+//             if (err) {
+//                 res.status(402).json({ message: "error", info: err.message })
+//             }
+//             if (decoded?.user_type !==  "air force user") {
+//                 res.status(401).send("Only  can create a new user")
+//             }
+//             req.user = decoded.id
+//         })
+//         next()
+//     } catch (error) {
+//         res.status(500).json({ message: "error", info: error.message })
+//     }
+// }
