bc2dcb4ff2176084ec318c644dec1cebb14de19f
Daniel Levy <d0583258844@gmail.com>
2026-03-17 19:16:28 +0200

    i try to add the pages logic,  with unseccess

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react"
+import "./Register.css"
+
 function RegisterPage() {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        email: "",
+        user_type: ""
+    })
+
+
+    async function handelSubmit(e) {
+        e.preventDefault()
+    }
+
+    function handelChang(e) {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
     return (
-        <div>
-            <p>Hello from Register Page</p>
+        <div className="mainregister">
+            <p className="title"> Hello Register Page</p>
+            <div className="allFields">
+                <form onSubmit={handelSubmit}>
+                    <label className="labelinput" htmlFor="username">userName: </label>
+                    <input onChange={handelChang} className="labelinput" type="text" id="username" name="username" />
+
+                    <label className="labelinput" htmlFor="password">password: </label>
+                    <input onChange={handelChang} className="labelinput" type="password" id="password" name="password" />
+
+                    <label className="labelinput" htmlFor="email">email: </label>
+                    <input onChange={handelChang} className="labelinput" type="email" id="email" name="email" />
+
+                    <label className="labelinput" htmlFor="usertype">user type: </label>
+                    <input onChange={handelChang} className="labelinput" type="text" id="usertype" name="usertype" />
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
